b821b63e6af43538552409028a9e9a6ff0f04058
Ram <rammahto645@gmail.com>
2025-06-15 16:01:51 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_DOT_E = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns a list of requirements from the given file.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("/n","") for req in requirements]  # .strip() removes \n
+
+        if HYPEN_DOT_E in requirements:
+            requirements.remove(HYPEN_DOT_E)
+
+    return requirements
+
+# ------------------- Setup function -------------------
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="Ram",
+    author_email="rammahto645@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
